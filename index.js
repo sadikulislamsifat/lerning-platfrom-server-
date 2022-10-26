2e3f919baf8a12f84ee6806287cdd28c235c7360
Sadikul Islam <sh.sifatallrafi.08@gmail.com>
2022-10-26 23:59:26 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const subjects = require('./subject.json')
+
+app.get('/', (req, res) =>{
+    res.send("API Running")
+});
+ 
+app.listen(port, () => {
+    console.log("Category running on port", port)
+});
+app.get('/subjects',  (req, res) => {
+    res.send(subjects)
+})
